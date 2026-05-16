770b3d4b1cbc8a9e285bad7ebd9f9122486db7a3
𝘭𝘶𝘤𝘢𝘴 𝘥𝘢𝘯𝘪𝘦𝘭 <lucasdanielvp@gmail.com>
2026-05-16 20:05:21 -0300

    Subindo novo portfólio atualizado

---
+++ port-new/script.js
@@ -0,0 +1,67 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
+
+// Executa assim que o DOM estiver pronto
+document.addEventListener("DOMContentLoaded", () => {
+  const preloaderText = document.querySelector(".preloader-text");
+  const preloader = document.getElementById("preloader");
+  const textos = document.querySelectorAll(".textoAnimado");
+  
+  let count = 0;
+
+  // 1. SIMULAÇÃO DO CONTADOR DO PRELOADER
+  const interval = setInterval(() => {
+    // Se a página já carregou completamente, vai direto para 100
+    if (document.readyState === "complete") {
+      count = 100;
+    } else {
+      count += Math.random() * 0.01;
+    }
+
+    if (count >= 100) {
+      count = 100;
+      clearInterval(interval);
+      
+      // Quando o contador chega a 100, chamamos a função que dispara a transição de saída e a entrada do site
+      iniciarAnimacoesSubsequentes();
+    }
+
+    preloaderText.textContent = `${Math.floor(count)}%`;
+  }, 40);
+
+  // 2. SEQUÊNCIA DE ANIMAÇÕES
+  function iniciarAnimacoesSubsequentes() {
+    const tl = gsap.timeline({
+      onComplete: () => {
+        preloader.remove();
+        ScrollTrigger.refresh();
+      }
+    });
+
+    tl.to(preloader, {
+      opacity: 0,
+      duration: 0.5,
+      ease: "power2.out"
+    });
+
+    tl.add(() => {
+      textos.forEach((texto) => {
+        const split = new SplitText(texto, { types: "lines, words, chars" });
+
+        gsap.from(split.chars, {
+          filter: "blur(20px)",
+          opacity: 0,
+          duration: 0.9,
+          stagger: {
+            each: 0.03,
+            from: "random",
+          },
+          scrollTrigger: {
+            trigger: texto,
+            start: "top 80%",
+            toggleActions: "play none restart none",
+          },
+        });
+      });
+    }, "-=0.3");
+  }
+});
